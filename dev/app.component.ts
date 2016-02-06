2425c45cb6e2fe221216a6884f1e2ff0807c96b0
tonirilix <jesusmontes.mx@hotmail.com>
2016-02-06 01:22:04 -0600

    Adding RouteConfig and using them instead of writing a selector

--- dev/app.component.ts
+++ dev/app.component.ts
@@ -1,13 +1,37 @@
 import {Component} from 'angular2/core';
 import {ContactListComponent} from './contacts/contact-list.component';
+import {NewContactComponent} from './contacts/new-contact.component';
+import {ROUTER_DIRECTIVES} from 'angular2/router';
+import {RouteConfig} from 'angular2/router';
 
 @Component({
     selector: 'app',
     template: `
-        <contact-list></contact-list>               
+        <header>
+            <nav>
+                <a [routerLink]="['Contacts']">Contacts</a>
+                <a [routerLink]="['NewContact']">New Contact</a>
+            </nav>
+        </header>
+        <div class="main">
+            <router-outlet></router-outlet>
+        </div>                       
     `,
-    directives: [ContactListComponent],    
+    directives: [ContactListComponent, ROUTER_DIRECTIVES],    
 })
+@RouteConfig([
+    {
+        path: '/contacts', 
+        name: 'Contacts',
+        component: ContactListComponent,
+        useAsDefault: true
+    },
+    {
+        path: '/newcontact', 
+        name: 'NewContact',
+        component: NewContactComponent 
+    }
+])
 export class AppComponent {
     
 }
