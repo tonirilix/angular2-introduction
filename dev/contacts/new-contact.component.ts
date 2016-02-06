0c8b8e1211839932d0caa502c9c9314df47de004
tonirilix <jesusmontes.mx@hotmail.com>
2016-02-06 01:50:56 -0600

    A really shitty way to get data from form. I should update this when i learn more about forms manipulation

--- dev/contacts/new-contact.component.ts
+++ dev/contacts/new-contact.component.ts
@@ -1,10 +1,49 @@
 import {Component} from "angular2/core";
+import {ContactService} from './contact.service';
+import {Contact} from './contact';
 
 @Component({    
     template: `        
-        Create a new contact
-    `
+        <div>
+            <div>
+            <label for="id">Id:</label>              
+            <input type="text" id="id" #id>
+            </div>
+            <div>
+            <label for="first-name">First Name:</label>              
+            <input type="text" id="first-name" #firstName>
+            </div>
+            <div>
+            <label for="last-name">Last Name:</label>                        
+            <input type="text" id="last-name" #lastName>
+            </div>
+            <div>
+            <label for="phone">Phone Number:</label>
+            <input type="text" id="phone" #phone>
+            </div>
+            <div>
+            <label for="email">Email:</label> 
+            <input type="text" id="email" #email>
+            </div>
+            <button (click)="onAddContact(id, firstName, lastName, phone, email)">Create contact</button>
+        </div>
+    `,
+    providers: [ContactService],
+    styles: [`
+        label {
+            display: inline-block;
+            width: 140px;
+        }
+        
+        input {
+            width: 250px;
+        }
+    `]
 })
-export class NewContactComponent {
-   public contact = {};
+export class NewContactComponent {   
+   constructor(private _contactService: ContactService){}
+   onAddContact(id, firstName, lastName, phone, email){
+       let contact: Contact = {id: id, firstName: firstName, lastName: lastName, phone: phone, email: email};
+       this._contactService.insertContact(contact);
+   }
 }
